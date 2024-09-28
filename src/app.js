90b4eb2d98aed3423542a306a8c6046a2f6cf07d
Mark Valentino <mike9377mike9377@gmail.com>
2024-09-28 00:23:17 -0400

    improved dir structure

--- src/app.js
+++ src/app.js
@@ -1,16 +1,24 @@
-const express = require('express');
-const path = require('path');
+const express = require("express");
+const path = require("path");
 
 const app = express();
 const port = 8080;
 
-app.use(express.static(path.join(__dirname, '/public')));
+// Serve static files from the public directory
+app.use(express.static(path.join(__dirname, "../public")));
 
+// Define the Hello World route
+app.get("/helloworld", function (req, res) {
+  res.send("Hello World!");
+});
+
+// Handle 404 errors
 app.use((req, res) => {
-  res.status(404);
-  res.sendFile(path.join(__dirname, '/public', '404.html'));
+  res.status(404).sendFile(path.join(__dirname, "../public", "404.html"));
 });
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+// Start the server
+app.listen(port, function () {
+  console.log(`Live on port: ${port}!`);
 });
+
